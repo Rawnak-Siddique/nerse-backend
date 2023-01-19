74f01fe843193a6f39bff2e0cc7b21942a6e8794
Rawnak-Siddique <g0urney0fth0u5andm11e5@gmail.com>
2023-01-19 17:15:09 +0600

    11th commit

--- Route/Routes.js
+++ Route/Routes.js
@@ -20,7 +20,7 @@ import { createsProfile,
          getRenter,
          CreateNurse,
          getNurse,
-         getSelectedNurse
+         getSelectedNurse,
          CreateDoctor,
          getDoctor,
          getSelectedDoctor,
